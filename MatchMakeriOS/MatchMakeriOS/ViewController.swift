ce9a002953e46afd32c4ad9b5e83ea251ee358d6
Jmay <justin.sean.may@gmail.com>
2018-05-24 19:28:36 -0400

    created the iOS app

---
+++ MatchMakeriOS/MatchMakeriOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MatchMakeriOS
+//
+//  Created by Justin May on 5/24/18.
+//  Copyright © 2018 Justin May. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
