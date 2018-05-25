6626ac3196ea3b6548787c35cc5b89f358d5c9e6
Jmay <justin.sean.may@gmail.com>
2018-05-24 23:26:22 -0400

    added title screen

--- MatchMakeriOS/MatchMakeriOS/ViewController.swift
+++ MatchMakeriOS/MatchMakeriOS/ViewController.swift
@@ -10,8 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleButtonLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        titleButtonLabel.textColor = UIColor(red:0.87, green:0.96, blue:0.27, alpha:1.0)
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
