bb4d3e30f6e1888f4955a7287d07d1987ca4a250
Anand Nanduri <an9878@gmail.com>
2018-06-16 19:23:10 -0400

    added authentication

--- mongoServer.js
+++ mongoServer.js
@@ -1,26 +1,31 @@
+// Initialize Mongo client
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://user1:0sJw8TTVXWdOfSys@tennismatches-shard-00-00-hwzsb.mongodb.net:27017,tennismatches-shard-00-01-hwzsb.mongodb.net:27017,tennismatches-shard-00-02-hwzsb.mongodb.net:27017/test?ssl=true&replicaSet=TennisMatches-shard-0&authSource=admin&retryWrites=true"
-var bodyParser = require('body-parser');
 
+// Import express
 var express = require('express');
 var app = express();
-   
+
+// Import hashing
+const bcrypt = require('bcrypt');
+
+// Formatting
+var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Create the server
+// Listening on port 3000
 var server = app.listen(3000, function(){
 	console.log("listening to requests on 3000");
 });
 
-app.post('/test', function(req,res){
-	console.log('hello');
-});
 
-//Register a new person
+// Register a new person
 app.post('/register', function(req,res){
 	var email = req.body.email;
 	var fname = req.body.fname;
 	var lname = req.body.lname;
-	var password = req.body.password; //hash this later
+	var password = bcrypt.hashSync(req.body.password, 10); //hash this later
 	// var age;
 	// var sex;
 	// var image;
@@ -35,16 +40,68 @@ app.post('/register', function(req,res){
 
 		var person = {email: email, fname: fname, lname: lname, password: password };
 
+		data.collection("TennisMatches").ensureIndex( { email: 1 }, { unique: true, sparse: true } )
 		data.collection("TennisMatches").insertOne(person, function(err, res){
 			if (err) throw err;
 			console.log("registered one person");
-			db.close();
 		});
 
+		db.close();
 	});
 
-	res.send("");
+	res.send(""); //send back unique ID
 });
 
+// Login user with email
+app.post('/login', function(req,res){
+	var email = req.body.email;
+	var fname = req.body.passAtt;
+	var passSav
+	var valid;
+
+	MongoClient.connect(url, function(err, db){
+		if (err) throw err;
+
+		var data = db.db("MatchMaker");
+
+		// Check if email exists
+		if((passSav = db.db("MatchMaker").findOne({email: email}, {password: 1})) == null){
+			valid = true;
+			db.close();
+			res.end(valid);
+		} 
+
+		// Check password
+		valid = bcrypt.compareSync(passAtt, passSav);
+
+		db.close();
+	});
+
+	res.send(valid);
+});
+
+
+// // Method to hash password with salt
+// function hashPass(password){
+// 	var hash;
+// 	var salt = crypto.randomBytes(64).toString('base64');
+//     var iterations = 10000;
+//     crypto.pbkdf2(password, salt, iterations, 64, 'sha512', (err, derivedKey) => {
+//   		if (err) throw err;
+//   		hash = derivedKey.toString('hex');
+
+//   		return {
+//         	salt: salt,
+//         	hash: hash,
+//         	iterations: iterations
+//     	};
+
+// 	});
+// }
+
+// // Method to check password attempt
+// function validatePass(savedHash, savedSalt, savedIterations, passwordAttempt){
+//     return savedHash == pbkdf2(passwordAttempt, savedSalt, savedIterations);
+// }
 
 
